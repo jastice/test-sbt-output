7a5bf084b8e0ee4785ad0245d1e13999922b7008
Justin Kaeser <justin.kaeser@jetbrains.com>
2021-10-01 19:51:27 +0200

    a test!

--- build.sbt
+++ build.sbt
@@ -3,3 +3,5 @@ name := "test-sbt-output"
 version := "0.1"
 
 scalaVersion := "2.13.6"
+
+libraryDependencies += "org.scalameta" %% "munit" % "0.7.29" % Test
--- src/main/scala/Blob.scala
+++ src/main/scala/Blob.scala
@@ -1,12 +1,12 @@
 object Blob {
 
   def ma = 23
-  def ga: Int = "nope"
+  def ga: Int = 1 //"nope"
   def wa = {
     val a = ma*2
     val l = List.empty[Int]
     l.foldLeft("haha") { (z, x) =>
-      z.toInt + x
+      z + x
     }
   }
 }
